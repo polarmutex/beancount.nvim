ba65fca49b8e0293747a271a1c6aea38517efaca
Brian Ryall <brianryall@fastmail.com>
2020-08-11 14:28:41 -0400

    add my highlight query file from my dotefiles

---
+++ queries/beancount/highlights.scm
@@ -0,0 +1,32 @@
+; Keywords
+[
+    "option"
+    "plugin"
+    "include"
+    "event"
+    "open"
+    "close"
+    "note"
+    "document"
+    "balance"
+    "price"
+    "pushtag"
+    "poptag"
+    "pad"
+ ] @keyword
+
+(date) @number
+(flag) @property
+
+(account) @type
+
+(amount) @number
+(incomplete_amount) @number
+(currency) @property
+
+(key) @label
+(string) @string
+
+(tag) @constant
+
+(comment) @comment
