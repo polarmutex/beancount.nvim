553a7f306d50770c55f2af67f021bd48fc743d9f
Brian Ryall <brian@brianryall.xyz>
2022-05-07 11:22:49 -0400

    fix: formatting

--- lua/beancount.lua
+++ lua/beancount.lua
@@ -1,16 +1,14 @@
-
 local M = {}
 
 function M.init()
-
-    require "nvim-treesitter".define_modules {
+    require("nvim-treesitter").define_modules({
         beancount = {
             module_path = "nvim-beancount.internal",
             is_supported = function(lang)
-                return lang == 'beancount'
-            end
-        }
-    }
+                return lang == "beancount"
+            end,
+        },
+    })
 end
 
 local function prepare_match(entry, kind)
@@ -32,7 +30,7 @@ local function reverse(t)
     local n = #t
     local i = 1
     while i < n do
-        t[i],t[n] = t[n],t[i]
+        t[i], t[n] = t[n], t[i]
         i = i + 1
         n = n - 1
     end
@@ -41,72 +39,72 @@ end
 function M.CopyTransaction(opts)
     opts = opts or {}
 
-    local has_nvim_treesitter, nvim_treesitter = pcall(require, 'nvim-treesitter')
+    local has_nvim_treesitter, nvim_treesitter = pcall(require, "nvim-treesitter")
     if not has_nvim_treesitter then
-        print('You need to install nvim-treesitter')
+        print("You need to install nvim-treesitter")
         return
     end
 
-    local parsers = require('nvim-treesitter.parsers')
+    local parsers = require("nvim-treesitter.parsers")
     if not parsers.has_parser() then
-        print('No parser for the current buffer')
+        print("No parser for the current buffer")
         return
     end
 
-    local has_telescope, _ = pcall(require, 'telescope')
+    local has_telescope, _ = pcall(require, "telescope")
     if not has_telescope then
-        print('telescope.nvim not installed')
+        print("telescope.nvim not installed")
         return
     end
 
-    local ts_locals = require('nvim-treesitter.locals')
+    local ts_locals = require("nvim-treesitter.locals")
     local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
 
     local results = {}
     for _, definitions in ipairs(ts_locals.get_definitions(bufnr)) do
         local entries = prepare_match(definitions)
         for _, entry in ipairs(entries) do
-            if(entry.kind == "transaction") then
+            if entry.kind == "transaction" then
                 table.insert(results, entry)
             end
         end
     end
 
     if vim.tbl_isempty(results) then
-        print('empty results')
+        print("empty results")
         return
     end
 
     reverse(results)
 
-    local pickers = require('telescope.pickers')
-    local finders = require('telescope.finders')
-    local make_entry = require('telescope.make_entry')
-    local sorters = require('telescope.sorters')
-    local previewers = require('telescope.previewers')
-    local conf = require('telescope.config').values
-    local actions = require('telescope.actions')
-    local action_state = require "telescope.actions.state"
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local make_entry = require("telescope.make_entry")
+    local sorters = require("telescope.sorters")
+    local previewers = require("telescope.previewers")
+    local conf = require("telescope.config").values
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
 
     pickers.new(opts, {
-        prompt    = 'Transactions',
-        finder    = finders.new_table {
+        prompt = "Transactions",
+        finder = finders.new_table({
             results = results,
-            entry_maker = make_entry.gen_from_treesitter(opts)
-        },
+            entry_maker = make_entry.gen_from_treesitter(opts),
+        }),
         previewer = conf.grep_previewer(opts),
-        sorter    = sorters.get_generic_fuzzy_sorter(),
-        attach_mappings = function( prompt_bufnr, map)
-            local insert_txn = function ()
+        sorter = sorters.get_generic_fuzzy_sorter(),
+        attach_mappings = function(prompt_bufnr, map)
+            local insert_txn = function()
                 local selection = action_state.get_selected_entry()
                 actions.close(prompt_bufnr)
 
-                local lines = vim.api.nvim_buf_get_lines(bufnr,selection.start,selection.finish,false)
+                local lines = vim.api.nvim_buf_get_lines(bufnr, selection.start, selection.finish, false)
 
                 local row, col = unpack(vim.api.nvim_win_get_cursor(0))
                 vim.api.nvim_buf_set_lines(bufnr, row, row, false, lines)
             end
-            map('i', '<CR>', insert_txn)
+            map("i", "<CR>", insert_txn)
             return true
         end,
     }):find()
