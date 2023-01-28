067e6a26a828437ecd72a250a656bf63d5a33d32
Brian Ryall <brian@brianryall.xyz>
2023-01-27 20:28:37 -0500

    fix: updated beancount tree-sitter query

--- lua/beancount.lua
+++ lua/beancount.lua
@@ -15,7 +15,7 @@ function M.init()
         'locals',
         [[(transaction
     (date) @definition.date
-    (txn_strings) @definition.payee) @definition.transaction
+    (narration) @definition.payee) @definition.transaction
 ]]
     )
 end
